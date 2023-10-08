d36a3c60c6809c300ca599ec907d905f28dedb8e
Семён Мордовский <118814265+PudgeAbuzer2007@users.noreply.github.com>
2023-10-08 12:03:51 +0500

    Add files via upload

--- lyceum/settings.py
+++ lyceum/settings.py
@@ -34,9 +34,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'homepage.apps.CatalogConfig',
+    'homepage.apps.HomepageConfig',
     'catalog.apps.CatalogConfig',
-    'about.apps.CatalogConfig',
+    'about.apps.AboutConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
