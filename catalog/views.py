499ec4f6111fb99636cd7eacf8fb69a6bebe9763
Семён Мордовский <118814265+PudgeAbuzer2007@users.noreply.github.com>
2023-10-12 21:50:19 +0500

    Add files via upload

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def item_list(request):
+    return HttpResponse("<body>Список элементов</body>")
+
+
+def item_detail(request, num):
+    return HttpResponse("<body>	Подробно элемент</body>")
